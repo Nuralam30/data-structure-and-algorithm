e47c3bc2bcafa608407431c28387f1a0a159fee6
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-29 23:41:30 +0600

    tree in js

---
+++ tree.js
@@ -0,0 +1,13 @@
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+  // usage
+  const root = new Node(2)
+  root.left = new Node(1)
+  root.right = new Node(3)
+  
