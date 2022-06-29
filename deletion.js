98b0fbdf80f738f04fea9cf78b328cd3a6582da6
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-29 22:27:24 +0600

    delete value from an array

---
+++ deletion.js
@@ -0,0 +1,15 @@
+
+// deletion.js
+var arr = [12, 13, 14, 15, 16, 17, 18, 19];
+
+arr.pop();
+console.log(arr)
+
+
+// delete from first 
+arr.shift();
+console.log(arr)
+
+// delete from a given index
+arr.splice(3,1);
+console.log(arr)
