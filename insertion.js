4caa5b8d97d44f64a0f7bed2f9c2e4546d1cacf9
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-29 22:17:54 +0600

    insert into before and after the given index

--- insertion.js
+++ insertion.js
@@ -1,7 +1,7 @@
 
 // insertion.js
 
-var arr = [13, 24, 23];
+var arr = [13, 24];
 
 arr.push(34);
 console.log(arr)
@@ -10,3 +10,21 @@ console.log(arr)
 // insertion at the begining
 arr.unshift(11);
 console.log(arr)
+
+
+// insert into a given index
+arr.splice(3,0, 30);
+console.log(arr)
+
+
+// insert into after the given index
+var givenIndex = 2;
+arr.splice((givenIndex +1),0, 35);
+console.log(arr)
+
+
+// insert into before the given index
+var givenIndex = 2;
+arr.splice((givenIndex - 1),0, 28);
+console.log(arr)
+
