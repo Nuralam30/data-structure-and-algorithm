a500a592f1b37c7f046bd5244bc09c32e05a7bff
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-01 12:07:59 +0600

    selection sort updated

--- selectionSort.js
+++ selectionSort.js
@@ -5,13 +5,13 @@ var arr = [45, 34, 87, 26, 76, 18, 57];
 
 for (let i=0; i<arr.length-1; i++){
     var k = i;
-    for (j=i+1; j<arr.length; j++){
+    for (let j=i+1; j<arr.length; j++){
         if(arr[j] < arr[k]){
             k = j;
-            var temp = arr[j];
-            arr[j] = arr[k];
-            arr[k] = temp;
         }
     }
+    var temp = arr[i];
+    arr[i] = arr[k];
+    arr[k] = temp;
 }
 console.log(arr)
