ecf035e64d38294db86f0608aecff65cb7b101a5
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-03 12:05:10 +0600

    first half and second half created

--- mergeSort.js
+++ mergeSort.js
@@ -3,10 +3,55 @@
 
 var arr = [45, 34, 87, 26, 76, 18, 57, 65];
 
-var firstHalf = Math.floor(arr.length / 2);
-var secondHalf = arr.length - firstHalf;
-
-for(var i=0; i<firstHalf.length-1; i++){
-    console.log(arr[i]);
+if(arr.length == 1){
+    return arr;
 }
-console.log(arr);
+
+
+var firstHalf = Math.floor(arr.length / 2);
+var firstStart = 0;
+
+var secondHalf = arr.length - firstHalf;
+var secondstart = arr.length - firstHalf;
+
+
+function halfArr(n, len){
+    var newArr = [];
+
+    if(len >= n){    
+        for (var i=0; i<n; i++){
+            for (var j=firstHalf; j<arr.length; j++){
+                if(arr[j] > arr[j+1]){
+                    var temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp; 
+                }
+            }
+        }
+            for(var i=firstHalf; i<arr.length; i++){
+                newArr.push(arr[i]);
+            }
+            console.log(newArr);
+    }
+
+    else{
+        for (var i=0; i<n; i++){
+            for (var j=0; j<n-1; j++){
+                if(arr[j] > arr[j+1]){
+                    var temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp; 
+                }
+            }
+        }
+        for(var i=0; i<n; i++){
+            newArr.push(arr[i]);
+        }
+        console.log(newArr);
+    }
+}
+
+
+var firstHalfArr = halfArr(firstHalf, firstStart)
+var secondHalfArr = halfArr(secondHalf, secondstart)
+
