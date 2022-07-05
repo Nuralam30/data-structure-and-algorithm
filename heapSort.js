5d9b3e80fd9fbf239ddad0c0294c8e34f3b5a8c0
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-05 17:03:40 +0600

    heap sort done

--- heapSort.js
+++ heapSort.js
@@ -1,37 +1,51 @@
 
 // heapSort.js
 
-var arr = [45, 34, 87, 26];
-var size = arr.length; 
+var arr = [45, 34, 87, 26, 76, 18, 57, 65, 95];
+var len; 
 
-function heapify(array, n) {
-    var par, node;
- 
-    for(var i=1; i<= n; i++) {
-       node = i; 
-       par = node/2;
+function heapify(array, i) {
+   var maxIndex = i;
+   var leftIndex = i*2 + 1;
+   var rigthIndex = i*2 + 2;
 
-       while(par >=1) {
-          if(array[par] < array[node]){
-            var temp = array[par];
-            array[par] = array[node];
-            array[node] = temp;
-          }
-        node = par;
-        par = node/2;
-       }
-    }
+   if(leftIndex < len && array[leftIndex] > array[maxIndex]){
+      maxIndex = leftIndex;
+   }
+
+   if(rigthIndex < len && array[rigthIndex] > array[maxIndex]){
+      maxIndex = rigthIndex;
+   }
+
+   if(maxIndex !== i){
+      swap(array, i, maxIndex);
+      heapify(array, maxIndex);
+   }
+ }
+
+
+ function swap(arr, index_1, index_2){
+   var temp = arr[index_1];
+   arr[index_1] = arr[index_2];
+   arr[index_2] = temp;
  }
 
  
- function heapSort(array, n) {
-    for(var i = n-1; i>= 1; i--) {
-       heapify(array, i); //heapify each time
-       var temp = array[0];
-       array[0] = array[i];
-       array[i] = temp; //swap last element with first
-    }
-    console.log(array)
+ function heapsort(array) {
+   len = array.length;
+   var heapIndex = Math.floor(len / 2);
+
+   for(var i = heapIndex; i>= 0; i--) {
+      heapify(array, i); //heapify each time
+   }
+
+   for (i = array.length-1; i>0; i--) {
+     swap(array, 0, i);
+     len--;
+   
+     heapify(array, 0);
+   }
+   console.log(array);
  }
 
- heapSort(arr, size);
+heapsort(arr);
